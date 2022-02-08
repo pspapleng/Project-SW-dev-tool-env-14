24af8e5b2926373b293d61cb5023f66900f43d95
ApichaJA <Junyatanakorn@gmail.com>
2022-02-08 20:03:34 +0700

    feat: Create a navbar and set route on page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,9 @@ import {
 
 //Import Page for Route
 import Home from './pages/Home/Home'
+import Assessment from './pages/Assessment/Assessment'
+import ServiceCenter from './pages/ServiceCenter/ServiceCenter'
+import FAQ from './pages/FAQ/FAQ'
 import NotFound from './pages/NotFound'
 
 //Import Component Navbar
@@ -14,6 +17,10 @@ import Navbar from './components/Navbar'
 const App = () => {
   let routes = useRoutes([
     { path: "/", element: <Home/> },
+    { path: "/Home", element: <Home/> },
+    { path: "/Assessment", element: <Assessment/> },
+    { path: "/ServiceCenter", element: <ServiceCenter/> },
+    { path: "/FAQ", element: <FAQ/> },
     { path: "*", element: <NotFound /> },
   ]);
   return routes;
--- front-end/src/pages/Home/Home.js
+++ front-end/src/pages/Home/Home.js
@@ -1,19 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import './Home.scss'
-import Button from '@mui/material/Button';
-
 
 function Home() {
-    const [text, setText] = useState(null)
-
-    const changeText = event => {
-        setText(event.target.value)
-    }
     return (
-      <div className='container'>
-          <input type="text" onChange={changeText} />
-          {text}
-          <Button variant="contained" onClick={() => alert('Why you Click')}>Confirm</Button>
+      <div>
+          Home
       </div>
     );
 }
