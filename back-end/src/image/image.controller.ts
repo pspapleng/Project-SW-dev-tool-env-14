d252eec6a4ea0362f22f75a68b7265914cf2aab0
pspapleng <chayanin3343@gmail.com>
2022-02-10 00:40:56 +0700

    fix: remove image module

--- back-end/src/image/image.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { ImageSerivce } from './image.service';
-
-@Controller('/image')
-export class ImageController {
-  constructor(private imageSerivce: ImageSerivce) {}
-}
