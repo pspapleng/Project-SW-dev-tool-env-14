22164ec52c77fce56def4143fd74c93d5eafc2c9
it62070040 <56312650+it62070040@users.noreply.github.com>
2022-02-11 15:40:14 +0700

    fix: remove controller follow review

--- back-end/src/image/image.controller.ts
+++ back-end/src/image/image.controller.ts
@@ -1,15 +1,7 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
-import { ImageSerivce } from './image.service';
+import { Controller } from "@nestjs/common";
+import { ImageSerivce } from "./image.service";
 
 @Controller('/image')
-export class ImageController {
-  constructor(private imageSerivce: ImageSerivce) {}
-@Post()
-async uploadImage(){
-  return 'uploadImage'
-  }
-@Get('/:id')
-async getImageById(@Param('id') id: string) {
-  return 'getImageById '+id    
-  }
+export class ImageController{
+    constructor(private imageSerivce: ImageSerivce){}
 }
