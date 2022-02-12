df5aab5f5563e28a478a5f04e1088482735f4e57
it62070041 <56312673+it62070041@users.noreply.github.com>
2022-02-12 16:51:25 +0700

    feat: add main page

--- front-end/src/components/Modals/MainPageModal.js
+++ front-end/src/components/Modals/MainPageModal.js
@@ -1,65 +1,81 @@
 import React from "react";
-import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
 import Modal from "@mui/material/Modal";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
+import { useNavigate } from 'react-router-dom';
+import { DarkGrayBut } from "../Button";
 
 function MainPageModal() {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(true);
   // const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-
+  let navigate = useNavigate();
+  const nextToAssessment = () =>{
+    navigate('/Assessment');
+  }
   return (
     <div>
-          <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
+      <Modal
+        open={open}
+        // onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        backdrop="static"
+        keyboard="false"
+      >
+        <Box
+          sx={{
+            width: 600,
+            height: "40vh",
+            bgcolor: "background.paper",
+            p: 4,
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            borderRadius: "15px",
+            textAlign: "center",
+          }}
+        >
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+            sx={{ color: "#595959", fontSize: 38, fontWeight: "bold" }}
           >
-            <Box
-              sx={{ width: 600, bgcolor: "background.paper", p: 4, position: 'absolute', top: '50%', left: '50%', borderRadius: '15px', textAlign: 'center'}}>
-              <Typography
-                id="modal-modal-title"
-                variant="h6"
-                component="h2"
-                sx={{ color: "#595959", fontSize: 38, fontWeight: "bold" }}
-              >
-                แบบประเมิน
-                <Divider
-                  sx={{
-                    mx: 27,
-                    color: "#595959",
-                    borderBottomWidth: "initial",
-                  }}
-                />
-              </Typography>
+            แบบประเมิน
+            <Divider
+              sx={{
+                mx: 27,
+                color: "#595959",
+                borderBottomWidth: "initial",
+              }}
+            />
+          </Typography>
+          <Grid id="top-row" container sx={{ justifyContent: "center" }}>
+            <Grid item xs={6}>
               <Typography
                 id="modal-modal-description"
                 sx={{ mt: 2, Color: "#595959", opacity: "0.67" }}
               >
-                คำอธิบายเกี่ยวกับแบบประเมิน
+                <p>คำอธิบายเกี่ยวกับแบบประเมิน</p>
               </Typography>
-              <Button
-                variant="contained"
-                sx={{
-                  borderRadius: "15px",
-                  marginTop: "10%",
-                  textTransform: "none",
-                  backgroundColor: "#595959",
-                  width: '30%',
-                  height: '20%',
-                  opacity: '0.67'
-                }}
-                className="button-modal"
-              >
-                Start an assessment
-              </Button>
-            </Box>
-          </Modal>
-        </div>
-    
+            </Grid>
+          </Grid>
+          <Grid
+            id="bottom-row"
+            container
+            sx={{ justifyContent: "center", mt: "20%" }}
+          >
+            <Grid item xs={6}>
+              <DarkGrayBut onClick={nextToAssessment}>Start an assessment</DarkGrayBut>
+            </Grid>
+          </Grid>
+        </Box>
+      </Modal>
+    </div>
   );
 }
 
