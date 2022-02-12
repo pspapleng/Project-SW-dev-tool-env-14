3deaf6ac996675f2d368db49eb12de3d90667d13
chulalak <56313629+chayanpx@users.noreply.github.com>
2022-02-12 16:18:06 +0700

    feat: getReviewByServiceCenterId

--- back-end/src/dal/review/review.repository.ts
+++ back-end/src/dal/review/review.repository.ts
@@ -2,4 +2,14 @@ import { EntityRepository, Repository } from 'typeorm';
 import { ReviewEntity } from './review.entity';
 
 @EntityRepository(ReviewEntity)
-export class ReviewRepository extends Repository<ReviewEntity> {}
+export class ReviewRepository extends Repository<ReviewEntity> {
+    async findByServiceCenterId(serviceCenterId: string): Promise<ReviewEntity[]> {
+        return await this
+            .find({
+                where:     {
+                    serviceCenter: serviceCenterId,
+                },
+                relations: ['user'],
+            })
+    }
+}
