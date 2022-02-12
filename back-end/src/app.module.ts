0a6d37a46e1c29667d0ea6395f79d531ff8607ec
pspapleng <chayanin3343@gmail.com>
2022-02-12 14:43:56 +0700

    update: remove all entity and import GlobalModule instead

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -3,12 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configService } from './config/config.service';
-import { FavoriteEntity } from './dal/favorite/favorite.entity';
-import { HappyBoxEntity } from './dal/happy-box/happy-box.entity';
-import { ReviewEntity } from './dal/review/review.entity';
-import { ServiceCenterEntity } from './dal/service-center/service-center.entity';
-import { UserEntity } from './dal/user/user.entity';
 import { FavoriteModule } from './favorite/favorite.module';
+import { GlobalModule } from './global.module';
 import { HappyBoxModule } from './happy-box/happy-box.module';
 import { ReviewModule } from './review/review.module';
 import { ServiceCenterModule } from './service-center/service-center.module';
@@ -17,14 +13,7 @@ import { UserModule } from './user/user.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
-    TypeOrmModule.forFeature([
-      UserEntity,
-      ServiceCenterEntity,
-      ReviewEntity,
-      FavoriteEntity,
-      HappyBoxEntity,
-    ]),
-
+    GlobalModule,
     UserModule,
     ServiceCenterModule,
     ReviewModule,
