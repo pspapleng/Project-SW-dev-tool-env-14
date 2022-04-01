442ad1e990cf3516312dd6b6201ec6f8cf5ef4a9
it62070040 <56312650+it62070040@users.noreply.github.com>
2022-04-01 23:41:49 +0700

    fix: component test

---
+++ front-end/cypress/integration/test.spec.js
@@ -0,0 +1,111 @@
+function fakeLocation(latitude, longitude) {
+    return {
+      onBeforeLoad(win) {
+        cy.stub(win.navigator.geolocation, "getCurrentPosition", (cb, err) => {
+          if (latitude && longitude) {
+            return cb({ coords: { latitude, longitude } });
+          }
+          throw err({ code: 1 });
+        });
+      },
+    };
+  }
+
+describe('Component Testing', () => {
+    beforeEach(() => {
+        cy.visit("/", fakeLocation(13.7120371, 100.7887341));
+
+        cy.server()
+    //    localStorage.setItem("name", 'arm')
+    //    localStorage.setItem("id", '6208861abb4fed735f833e07')
+       cy.intercept(
+         {
+            method: "GET",
+            url: `/service_center/location?lat=${13.7120371}&lon=${100.7887341}`,
+         },
+         [{
+            "id": "31a26fa9-55db-4059-b4a3-4c59de3f9b5b",
+          "createAt": "2022-02-10T16:19:25.359Z",
+          "updateAt": "2022-02-10T16:19:25.359Z",
+          "deleteAt": null,
+          "name": "OneManCounselor",
+          "description": "ปรึกษาปัญหาชีวิตกับนักจิตวิทยา เพื่อเข้าใจสาเหตุของปัญหาที่แท้จริง จนสามารถปรับวิธีคิด และปรับแนวทางการใช้ชีวิตให้ลงตัวยิ่งขึ้น คลี่คลายปัญหาภายในจิตใจ และกลับมามีความสุขอีกครั้ง",
+          "imageUrl": "https://341682-1054383-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2019/10/OneManCounselor.jpg",
+          "type": "BOTH",
+          "address": "อาคารวนาเพลส ชั้น1 (ฝั่งลานจอดรถด้านข้างอาคาร) \nซอยอยู่เจริญ16 ถนนรัชดาภิเษก (ซอยรัชดาภิเษก3) เขตดินแดง",
+          "province": "กรุงเทพมหานคร",
+          "website": "https://www.onemancounselor.com/",
+          "facebook": "https://www.facebook.com/onemancounselor",
+          "phone": "-",
+          "email": "onemancounselor@gmail.com",
+          "office_hours": "ทุกวัน ตามการนัดหมายที่สะดวก พบตัว (งดปรึกษาพบตัวชั่วคราวในช่วงสถานการณ์โควิด) เวลา 10.00 – 18.00 น. โทรศัพท์ เวลา 10.00 – 20.00 น. ออนไลน์(วิดีโอคอล หรือแชท) เวลา 10.00 – 20.00 น.",
+          "cost": "1300฿ - 3000฿",
+          "latitude": "13.77396306",
+          "longitude": "100.5651552"
+       },
+      ] 
+       ).as('getServiceCenterByLocation') 
+       cy.intercept(
+         {
+            method: "GET",
+            path: `/service_center/**`,
+         },
+         [{
+            "id": "31a26fa9-55db-4059-b4a3-4c59de3f9b5b",
+            "createAt": "2022-02-10T16:19:25.359Z",
+            "updateAt": "2022-02-10T16:19:25.359Z",
+            "deleteAt": null,
+            "name": "OneManCounselor",
+            "description": "ปรึกษาปัญหาชีวิตกับนักจิตวิทยา เพื่อเข้าใจสาเหตุของปัญหาที่แท้จริง จนสามารถปรับวิธีคิด และปรับแนวทางการใช้ชีวิตให้ลงตัวยิ่งขึ้น คลี่คลายปัญหาภายในจิตใจ และกลับมามีความสุขอีกครั้ง",
+            "imageUrl": "https://341682-1054383-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2019/10/OneManCounselor.jpg",
+            "type": "BOTH",
+            "address": "อาคารวนาเพลส ชั้น1 (ฝั่งลานจอดรถด้านข้างอาคาร) \nซอยอยู่เจริญ16 ถนนรัชดาภิเษก (ซอยรัชดาภิเษก3) เขตดินแดง",
+            "province": "กรุงเทพมหานคร",
+            "website": "https://www.onemancounselor.com/",
+            "facebook": "https://www.facebook.com/onemancounselor",
+            "phone": "-",
+            "email": "onemancounselor@gmail.com",
+            "office_hours": "ทุกวัน ตามการนัดหมายที่สะดวก พบตัว (งดปรึกษาพบตัวชั่วคราวในช่วงสถานการณ์โควิด) เวลา 10.00 – 18.00 น. โทรศัพท์ เวลา 10.00 – 20.00 น. ออนไลน์(วิดีโอคอล หรือแชท) เวลา 10.00 – 20.00 น.",
+            "cost": "1300฿ - 3000฿",
+            "latitude": "13.77396306",
+            "longitude": "100.5651552"
+       },
+       ] 
+       ).as('getServiceCenterById') 
+       
+     })
+    
+     it('get correct page (Home -> Assessment -> ServiceCenterInfo) and get API getServiceCenterByLocation', () => {
+        cy.location().should((loc) => {
+            expect(loc.pathname).to.eq("/");
+          });
+      
+          cy.get("button").contains("Start an assessment").click();
+      
+          cy.location().should((loc) => {
+            expect(loc.pathname).to.eq("/Assessment");
+          });
+      
+          cy.get("button").contains("ยอมรับ").click();
+      
+          cy.get('[type="radio"]').eq(1).check();
+      
+          cy.get('[type="radio"]').eq(3).check();
+      
+          cy.get("button").contains("Submit").click();
+      
+          cy.contains("การแปลผลประเมิน");   
+          
+          cy.get(
+            '[class="MuiPaper-root MuiPaper-elevation MuiPaper-rounded MuiPaper-elevation1 MuiCard-root css-j0g0od-MuiPaper-root-MuiCard-root"]'
+          )
+      
+            .should("have.length", 2)
+            .eq(0)
+            .click();
+      
+          cy.url().should('include', 'ServiceCenterInfo')
+            
+     })
+     
+   })
