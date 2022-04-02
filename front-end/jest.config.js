b1cea9e8586c31a30cbb57ab76c88f233ff0cbb7
Kanapakan <56270434+Kanapakan@users.noreply.github.com>
2022-04-02 22:50:39 +0700

    update: jenkins file

--- front-end/jest.config.js
+++ front-end/jest.config.js
@@ -1,11 +1,21 @@
 module.exports = {
-    setupFilesAfterEnv: ['./src/configs/enzyme.js'],
-    "moduleNameMapper": {
-      ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "identity-obj-proxy"
-    },
-      "globals": {
-        "window": {}
+  setupFilesAfterEnv: ["./configs/enzyme.js"],
+  moduleNameMapper: {
+    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
+      "identity-obj-proxy",
+  },
+  globals: {
+    window: {},
+  },
+  rootDir: "src",
+  reporters: [
+    "default",
+    [
+      "../node_modules/jest-html-reporter",
+      {
+        pageTitle: "Unit Test Report",
+        outputPath: "./coverage/report.html",
       },
-      
-      
-  };
+    ],
+  ],
+};
