ff0651d362af822f47761f4fa0953443b1f4697f
it62070040 <56312650+it62070040@users.noreply.github.com>
2022-04-02 22:28:29 +0700

    fix: component test

--- front-end/cypress/integration/component.spec.js
+++ front-end/cypress/integration/component.spec.js
@@ -11,73 +11,75 @@ function fakeLocation(latitude, longitude) {
   };
 }
 
-describe("Component Testing", function () {
-  beforeEach(() => {
+beforeEach(() => {
     
-    cy.visit("/", fakeLocation(13.7120371, 100.7887341));
+  cy.visit("/", fakeLocation(13.7120371, 100.7887341));
 
-    cy.server()
-
-    cy.intercept(
+  cy.intercept(
+    {
+      method: "GET",
+      url: `/service_center/location?lat=${13.7120371}&lon=${100.7887341}`,
+    },
+    [
       {
-        method: "GET",
-        url: `/service_center/location?lat=${13.7120371}&lon=${100.7887341}`,
-      },
-      [
-        {
-          "id": "31a26fa9-55db-4059-b4a3-4c59de3f9b5b",
-          "createAt": "2022-02-10T16:19:25.359Z",
-          "updateAt": "2022-02-10T16:19:25.359Z",
-          "deleteAt": null,
-          "name": "OneManCounselor",
-          "description": "ปรึกษาปัญหาชีวิตกับนักจิตวิทยา เพื่อเข้าใจสาเหตุของปัญหาที่แท้จริง จนสามารถปรับวิธีคิด และปรับแนวทางการใช้ชีวิตให้ลงตัวยิ่งขึ้น คลี่คลายปัญหาภายในจิตใจ และกลับมามีความสุขอีกครั้ง",
-          "imageUrl": "https://341682-1054383-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2019/10/OneManCounselor.jpg",
-          "type": "BOTH",
-          "address": "อาคารวนาเพลส ชั้น1 (ฝั่งลานจอดรถด้านข้างอาคาร) \nซอยอยู่เจริญ16 ถนนรัชดาภิเษก (ซอยรัชดาภิเษก3) เขตดินแดง",
-          "province": "กรุงเทพมหานคร",
-          "website": "https://www.onemancounselor.com/",
-          "facebook": "https://www.facebook.com/onemancounselor",
-          "phone": "-",
-          "email": "onemancounselor@gmail.com",
-          "office_hours": "ทุกวัน ตามการนัดหมายที่สะดวก พบตัว (งดปรึกษาพบตัวชั่วคราวในช่วงสถานการณ์โควิด) เวลา 10.00 – 18.00 น. โทรศัพท์ เวลา 10.00 – 20.00 น. ออนไลน์(วิดีโอคอล หรือแชท) เวลา 10.00 – 20.00 น.",
-          "cost": "1300฿ - 3000฿",
-          "latitude": "13.77396306",
-          "longitude": "100.5651552"
-      }   
-      ]
-    ).as("getServiceCenterByLocation");
+        "id": "31a26fa9-55db-4059-b4a3-4c59de3f9b5b",
+        "createAt": "2022-02-10T16:19:25.359Z",
+        "updateAt": "2022-02-10T16:19:25.359Z",
+        "deleteAt": null,
+        "name": "OneManCounselor",
+        "description": "ปรึกษาปัญหาชีวิตกับนักจิตวิทยา เพื่อเข้าใจสาเหตุของปัญหาที่แท้จริง จนสามารถปรับวิธีคิด และปรับแนวทางการใช้ชีวิตให้ลงตัวยิ่งขึ้น คลี่คลายปัญหาภายในจิตใจ และกลับมามีความสุขอีกครั้ง",
+        "imageUrl": "https://341682-1054383-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2019/10/OneManCounselor.jpg",
+        "type": "BOTH",
+        "address": "อาคารวนาเพลส ชั้น1 (ฝั่งลานจอดรถด้านข้างอาคาร) \nซอยอยู่เจริญ16 ถนนรัชดาภิเษก (ซอยรัชดาภิเษก3) เขตดินแดง",
+        "province": "กรุงเทพมหานคร",
+        "website": "https://www.onemancounselor.com/",
+        "facebook": "https://www.facebook.com/onemancounselor",
+        "phone": "-",
+        "email": "onemancounselor@gmail.com",
+        "office_hours": "ทุกวัน ตามการนัดหมายที่สะดวก พบตัว (งดปรึกษาพบตัวชั่วคราวในช่วงสถานการณ์โควิด) เวลา 10.00 – 18.00 น. โทรศัพท์ เวลา 10.00 – 20.00 น. ออนไลน์(วิดีโอคอล หรือแชท) เวลา 10.00 – 20.00 น.",
+        "cost": "1300฿ - 3000฿",
+        "latitude": "13.77396306",
+        "longitude": "100.5651552"
+    }   
+    ]
+  ).as("getServiceCenterByLocation");
 
-   cy.intercept(
+ cy.intercept(
+    {
+      method: "GET",
+      path: `/service_center/**`,
+    },
+    [
       {
-        method: "GET",
-        path: `/service_center/**`,
-      },
-      [
-        {
-          "id": "31a26fa9-55db-4059-b4a3-4c59de3f9b5b",
-          "createAt": "2022-02-10T16:19:25.359Z",
-          "updateAt": "2022-02-10T16:19:25.359Z",
-          "deleteAt": null,
-          "name": "OneManCounselor",
-          "description": "ปรึกษาปัญหาชีวิตกับนักจิตวิทยา เพื่อเข้าใจสาเหตุของปัญหาที่แท้จริง จนสามารถปรับวิธีคิด และปรับแนวทางการใช้ชีวิตให้ลงตัวยิ่งขึ้น คลี่คลายปัญหาภายในจิตใจ และกลับมามีความสุขอีกครั้ง",
-          "imageUrl": "https://341682-1054383-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2019/10/OneManCounselor.jpg",
-          "type": "BOTH",
-          "address": "อาคารวนาเพลส ชั้น1 (ฝั่งลานจอดรถด้านข้างอาคาร) \nซอยอยู่เจริญ16 ถนนรัชดาภิเษก (ซอยรัชดาภิเษก3) เขตดินแดง",
-          "province": "กรุงเทพมหานคร",
-          "website": "https://www.onemancounselor.com/",
-          "facebook": "https://www.facebook.com/onemancounselor",
-          "phone": "-",
-          "email": "onemancounselor@gmail.com",
-          "office_hours": "ทุกวัน ตามการนัดหมายที่สะดวก พบตัว (งดปรึกษาพบตัวชั่วคราวในช่วงสถานการณ์โควิด) เวลา 10.00 – 18.00 น. โทรศัพท์ เวลา 10.00 – 20.00 น. ออนไลน์(วิดีโอคอล หรือแชท) เวลา 10.00 – 20.00 น.",
-          "cost": "1300฿ - 3000฿",
-          "latitude": "13.77396306",
-          "longitude": "100.5651552"
-      }
-      ]
-    ).as("getServiceCenterById");
+        "id": "31a26fa9-55db-4059-b4a3-4c59de3f9b5b",
+        "createAt": "2022-02-10T16:19:25.359Z",
+        "updateAt": "2022-02-10T16:19:25.359Z",
+        "deleteAt": null,
+        "name": "OneManCounselor",
+        "description": "ปรึกษาปัญหาชีวิตกับนักจิตวิทยา เพื่อเข้าใจสาเหตุของปัญหาที่แท้จริง จนสามารถปรับวิธีคิด และปรับแนวทางการใช้ชีวิตให้ลงตัวยิ่งขึ้น คลี่คลายปัญหาภายในจิตใจ และกลับมามีความสุขอีกครั้ง",
+        "imageUrl": "https://341682-1054383-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2019/10/OneManCounselor.jpg",
+        "type": "BOTH",
+        "address": "อาคารวนาเพลส ชั้น1 (ฝั่งลานจอดรถด้านข้างอาคาร) \nซอยอยู่เจริญ16 ถนนรัชดาภิเษก (ซอยรัชดาภิเษก3) เขตดินแดง",
+        "province": "กรุงเทพมหานคร",
+        "website": "https://www.onemancounselor.com/",
+        "facebook": "https://www.facebook.com/onemancounselor",
+        "phone": "-",
+        "email": "onemancounselor@gmail.com",
+        "office_hours": "ทุกวัน ตามการนัดหมายที่สะดวก พบตัว (งดปรึกษาพบตัวชั่วคราวในช่วงสถานการณ์โควิด) เวลา 10.00 – 18.00 น. โทรศัพท์ เวลา 10.00 – 20.00 น. ออนไลน์(วิดีโอคอล หรือแชท) เวลา 10.00 – 20.00 น.",
+        "cost": "1300฿ - 3000฿",
+        "latitude": "13.77396306",
+        "longitude": "100.5651552"
+    }
+    ]
+  ).as("getServiceCenterById");
 
-  });
+});
 
+afterEach(() => {
+  cy.clearLocalStorage()
+})
+
+describe("Component Testing", function () {
   // //สถานที่ให้บริการที่แนะนำลำกับที่ 1 และเป็นการตอบแบบสอบถามด้วยผู้ใช้ที่ไม่มีความเสี่ยง
   it("get correct page (Home -> Assessment -> ServiceCenterInfo) and get API getServiceCenterByLocation", function () {
 
@@ -114,6 +116,10 @@ describe("Component Testing", function () {
     
   });
 
+
+});
+describe("get correct id from assessment to service center", function () {
+  
   it("get correct id from assessment to service center", function () {
     cy.location().should((loc) => {
       expect(loc.pathname).to.eq("/");
@@ -152,4 +158,6 @@ describe("Component Testing", function () {
      
     });
   });
+
 });
+
